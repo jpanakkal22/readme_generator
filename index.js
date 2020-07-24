ce95bca6c96bc6780b45694ec03e6931f14e7b5b
New Dad <josh_panakkal3122@hotmail.com>
2020-07-24 05:27:35 -0400

    inquirer user prompts

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
 
-];
+// array of questions for user
+const questions = [{
+    title: "What is the title of your project?",
+    description: "Enter a description of your project",
+    installation: "Enter any installation instructions",
+    usage: "Enter usage information",
+    contribution: "Enter contribution guidelines",
+    test: "Enter test instructions"
+}];
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -9,6 +16,45 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer
+    .prompt([
+        {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+        },
+        {
+        type: "input",
+        message: "Enter a description of your project",
+        name: "description"
+        },
+        {
+        type: "input",
+        message: "Enter any installation instructions",
+        name: "installation"
+        },
+        {
+        type: "input",
+        message: "Enter usage information",
+        name: "usage"
+        },
+        {
+        type: "input",
+        message: "Enter contribution guidelines",
+        name: "contribution"  
+        },
+        {
+        type: "input",
+        message: "Enter test instructions",
+        name: "test"  
+
+        }
+    ])
+    .then(function(response) {
+
+        console.log(JSON.stringify(response));
+    });
+
 
 }
 
